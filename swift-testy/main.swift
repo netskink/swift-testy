76d0beb15dcfed39308b8de47f36bd61134bbaf1
John F. Davis <davisjf@gmail.com>
2015-04-20 10:46:03 -0600

    Initial commit from local git

---
+++ swift-testy/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  swift-testy
+//
+//  Created by John Fred Davis on 4/20/15.
+//  Copyright (c) 2015 John Fred Davis. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
